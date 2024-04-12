c9746b640699bf2ee9e9b2e1a70c78b0fcbac2db
DrinoSan <s.becirbegovic@gmail.com>
2024-04-13 01:33:06 +0200

    Little play with log

--- Log.cpp
+++ Log.cpp
@@ -10,10 +10,11 @@ std::shared_ptr<spdlog::logger> Log_t::s_CoreLogger;
 
 void Log_t::init()
 {
-    spdlog::set_pattern( "%^[%T] %n: %v%$" );
+    //spdlog::set_pattern( "%^[%T] %n: %v%$" );
+    //spdlog::set_pattern("[%H:%M:%S %z] [%^%L%$] [thread %t] %v");
 
     // This is set for logging function name line and file
-    // spdlog::set_pattern( "%^[%T] %n: %v%$ [%s] [%!: %#]" );
+    spdlog::set_pattern( "%^[%T] %n: %v%$ [%s] [%!: %#]" );
 
     s_CoreLogger = spdlog::stdout_color_mt( "SandServer" );
     s_CoreLogger->set_level( spdlog::level::trace );
