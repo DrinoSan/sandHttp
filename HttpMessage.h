e35a5d606b1c938f587a3572bbdeac00e14ac2e0
DrinoSan <s.becirbegovic@gmail.com>
2024-05-26 22:22:06 +0200

    Clangd

--- HttpMessage.h
+++ HttpMessage.h
@@ -11,12 +11,18 @@ namespace SandServer
 class HTTPMessage_t
 {
   public:
-    HTTPMessage_t() = default;
     virtual ~HTTPMessage_t() = default;
 
     void        setHeader( const std::string& name, const std::string& value );
+
+    [[nodiscard]]
     std::string getHeader( const std::string& name ) const;
-    void        setBody( const std::string& body );
+    inline void        setBody( const std::string& body_ )
+    {
+      body = body_;
+    }
+
+    [[nodiscard]]
     std::string getBody() const;
 
     void         printHeaders();
@@ -31,14 +37,17 @@ class HTTPMessage_t
 class HTTPRequest_t : public HTTPMessage_t
 {
   public:
-    HTTPRequest_t() = default;
-    virtual ~HTTPRequest_t() = default;
+     ~HTTPRequest_t() override = default;
 
     // Specific methods
     inline void setMethod( const std::string& method_ ) { method = method_; }
+    
+    [[nodiscard]]
     inline std::string getMethod() const { return method; };
 
     inline void        setURI( const std::string& uri_ ) { uri = uri_; }
+
+    [[nodiscard]]
     inline std::string getURI() const { return uri; }
 
     inline void setVersion( const std::string& version_ )
@@ -46,7 +55,8 @@ class HTTPRequest_t : public HTTPMessage_t
         version = version_;
     }
 
-    inline std::string getVersion() const { return version; }
+    [[nodiscard]]
+    inline auto getVersion() const -> std::string { return version; }
 
     void printObject() override;
 
@@ -65,11 +75,13 @@ class HTTPResponse_t : public HTTPMessage_t
 
     // Specific methods
     void setStatusCode( int32_t statusCode_ ) { statusCode = statusCode_; }
-    inline int  getStatusCode() const { return statusCode; }
+    [[nodiscard]] inline int  getStatusCode() const { return statusCode; }
     void        setReasonPhrase( const std::string& reasonPhrase );
-    std::string getReasonPhrase() const;
+    [[nodiscard]] std::string getReasonPhrase() const;
 
     void printObject() override;
+
+    static HTTPResponse_t notFound();
 
   private:
     int32_t statusCode;
